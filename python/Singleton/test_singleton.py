e42d9a1d915d0f94b48d275da70a0b15ad41c84f
Taro Hoshino <no_taro1990@outlook.com>
2019-02-23 19:13:49 +0900

    テストコード importの変更
    外部pythonからの実行、直接の実行(python Singleton/test_singleton.py)の両方に対応するため

--- python/Singleton/test_singleton.py
+++ python/Singleton/test_singleton.py
@@ -1,5 +1,9 @@
 import unittest
-from .singleton import Singleton
+import os
+import sys
+
+sys.path.append(os.path.dirname(__file__))
+from singleton import Singleton
 
 class TestSingleton(unittest.TestCase):
     """Singleton __new__ テスト
