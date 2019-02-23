f6dde5a907def8d812fd8c70a71630af29dbec1b
Taro Hoshino <no_taro1990@outlook.com>
2019-02-23 16:02:37 +0900

    UMLのPNG作成用バッチファイル修正

--- generate_png_of_uml.bat
+++ generate_png_of_uml.bat
@@ -1,8 +1,7 @@
 @echo off
-rem 当バッチファイルの第1引数(%1)にはDesignePatternLabフォルダの絶対ディレクトリパスを指定すること
 
 rem java -jar <plantuml.jarのパス> -o <出力先ディレクトリ> <入力元パス>
-java -jar %1\lib\plantuml.jar -o %1\uml %1\uml\**.txt
+java -jar %~dp0plantuml.jar -o %~dp0uml %~dp0uml\**.txt
 
 rem 参考1: http://plantuml.com/command-line
 rem 参考2: http://yohshiy.blog.fc2.com/blog-entry-152.html
